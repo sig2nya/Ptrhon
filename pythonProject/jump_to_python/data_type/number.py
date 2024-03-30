f46b27996f727f026eee6d2d7a1b581fa80c8f3d
홍민식 <hongminsik@hongminsig-ui-iMac.local>
2024-03-30 12:13:17 +0900

    init

---
+++ pythonProject/jump_to_python/data_type/number.py
@@ -0,0 +1,30 @@
+# ch1) Integer
+a = 123
+print(a)
+
+a = -178
+print(a)
+
+a = 0
+print(a)
+
+# ch2) float
+a = 1.2
+print(a)
+
+a = 3.45
+print(a)
+
+a = 4.24E10 # 4.24 * 10^10
+print(a)
+
+a = 4.24e-10 # 4.24 * 10^-10
+print(a)
+
+# ch3) octa / hexadecimal
+a = 0o177
+print(a) # 127
+
+a = 0x8ff
+b = 0xABC
+print(b) # 2748
---
+++ pythonProject/jump_to_python/data_type/operand.py
@@ -0,0 +1,29 @@
+# ch1) 사칙연산
+a = 3
+b = 4
+print(a + b)
+print(a - b)
+print(a * b)
+print(a / b)
+
+# ch2) 제곱
+print(a ** b) # 3^4 = 81
+
+# ch3) 나머지 연산자
+print(7 % 3)
+print(3 % 7)
+
+# ch4) 몫
+print(7 / 4) # 일반적인 나누기
+print(7 // 4) # 나눈 후, 몫을 리턴
+
+# ch5) 복합 연산자
+a = 1
+a = a + 1
+print(a)
+
+a += 1
+print(a)
+
+a -= 1
+print(a)
